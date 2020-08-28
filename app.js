dd9f7ba3d6662c90d4b1213d5e63caacc4b60d23
ankitsejwal <ankit@sejw.al>
2020-08-28 17:24:04 +1000

    add pug as the view engine

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const home = require("./routes/home");
 const users = require("./routes/users");
 const shifts = require("./routes/shifts");
 const bodyParser = require("body-parser");
+const pug = require("pug");
+const joi = require("joi");
 
 const app = express();
 
 // middlewares
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.use(bodyParser.json());
 
+// routes middlewares
+app.use("/", home);
 app.use("/api/users", users);
 app.use("/api/shifts", shifts);
 
